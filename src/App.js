b0411b33a2a2cf738a4b5a828b6ca3343f3453f4
Wjozey <66113453+Wjozey@users.noreply.github.com>
2023-10-16 12:20:19 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>better work</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {/* <p>{`This is my name : ${process.env.REACT_APP.NAME}`}</p> */}
+        <p>Testing again</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
